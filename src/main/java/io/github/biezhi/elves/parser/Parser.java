26ad48b078dc6badb4a6c816a16cd381ce0de3db
biezhi <biezhi.me@gmail.com>
2018-01-11 22:42:28 +0800

    :fire: remove parser

--- src/main/java/io/github/biezhi/elves/parser/Parser.java
+++
@@ -1,8 +0,0 @@
-package io.github.biezhi.elves.parser;
-
-/**
- * @author biezhi
- * @date 2018/1/11
- */
-public interface Parser {
-}
--- src/test/java/io/github/biezhi/elves/examples/News163Example.java
+++ src/test/java/io/github/biezhi/elves/examples/News163Example.java
@@ -1,7 +1,6 @@
 package io.github.biezhi.elves.examples;
 
 import io.github.biezhi.elves.Elves;
-import io.github.biezhi.elves.parser.Parser;
 import io.github.biezhi.elves.spider.News163Spider;
 
 /**
@@ -14,12 +13,9 @@ public class News163Example {
 
     public static void main(String[] args) {
         News163Spider news163Spider = new News163Spider("网易新闻");
-        news163Spider.startUrls("http://news.163.com/rank").parser(new News163Parser());
+        news163Spider.startUrls("http://news.163.com/rank");
 
         Elves.me(news163Spider).start();
     }
 
-    static class News163Parser implements Parser {
-
-    }
 }

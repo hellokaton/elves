8fec1ca2bddab093b12326a04363f51804ea7378
biezhi <biezhi.me@gmail.com>
2018-01-11 21:46:28 +0800

    :tada: initial commit

---
+++ src/main/java/io/github/biezhi/elves/Elves.java
@@ -0,0 +1,11 @@
+package io.github.biezhi.elves;
+
+/**
+ * @author biezhi
+ * @date 2018/1/11
+ */
+public class Elves {
+    public static void main(String[] args) {
+        Runnable r = ()->{};
+    }
+}
